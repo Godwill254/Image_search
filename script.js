7e4ba811eb8a737f86b38dfc1c69265f3577877d
Godwill254 <simiyugodwill540@gmail.com>
2024-02-15 20:35:15 +0300

    1.2

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ async function searchImages() {
     //Taking the value input element and storing it in inputData variable
     inputData = inputEl.value;
     
-    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}'
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
 
     const response = await fetch(url);
     const data = await response.json()
@@ -55,7 +55,7 @@ async function searchImages() {
 }
 
 formEl.addEventListener("submit", (event) => {
-    event.preventDefault;
+    event.preventDefault();
     page = 1;
     searchImages();
 })
